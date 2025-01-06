5cad368f3e092a66e69b78367bb7fb47a99c9ca9
stsnsn <satoshi-nishino@g.ecc.u-tokyo.ac.jp>
2025-01-06 20:44:11 +0900

    new file:   css/hue_roatte_icon.css

--- js/genome_map.js
+++ js/genome_map.js
@@ -6,6 +6,38 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// MarkerClusterGroupの作成
+const markers = L.markerClusterGroup();
+
+// typeに基づくアイコン色
+function getIconByType(type) {
+    let iconClass;
+
+    // typeに応じてアイコンのクラスを変更
+    switch (type) {
+        case 'MAG':
+            iconClass = 'acmarker_red'; // MAGの場合は赤いアイコン
+            break;
+        case 'SAG':
+            iconClass = 'acmarker_blue'; // SAGの場合は青いアイコン
+            break;
+        case 'Pure_culture':
+            iconClass = 'acmarker_yellow'; // Pure_cultureの場合は黄色いアイコン
+            break;
+        default:
+            iconClass = 'acmarker_gray'; // その他の場合は緑色のアイコン
+            break;
+    }
+
+    return L.icon({
+        iconUrl: '../css/images/marker-icon.png', // アイコン画像
+        iconSize: [25, 41], // アイコンサイズ
+        iconAnchor: [12, 41], // アイコンのアンカー位置
+        popupAnchor: [1, -34], // ポップアップのアンカー位置
+        className: iconClass // アイコンにクラスを追加
+    });
+}
+
 // TSVファイルのパス
 const tsvFilePath = '../data/subclade.txt'; // ここにTSVファイルのパスを指定
 
@@ -23,7 +55,10 @@ Papa.parse(tsvFilePath, {
 
             // 緯度経度が有効であればマーカーを追加
             if (!isNaN(latitude) && !isNaN(longitude)) {
-                const marker = L.marker([latitude, longitude]).addTo(map);
+                // マーカーを作成
+                const marker = L.marker([latitude, longitude], {
+                    icon: getIconByType(row.type) // typeに基づいてアイコンを設定
+                });
 
                 // ポップアップに表示する内容を動的に生成
                 let popupContent = '<div class="popup-content">';
@@ -36,10 +71,17 @@ Papa.parse(tsvFilePath, {
 
                 // クリック時にポップアップを表示
                 marker.bindPopup(popupContent);
+
+                // MarkerClusterGroupにマーカーを追加
+                markers.addLayer(marker);
             }
         });
+
+        // マーカーを地図に追加
+        map.addLayer(markers);
     },
     error: function(error) {
         console.error('TSV file loading error:', error);
     }
 });
+
